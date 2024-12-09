e92f4c0ce525782ede8386bf4c56a57c1114935e
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-12-09 19:36:24 -0300

    feat: creating repositorys

---
+++ src/main/java/br/com/pablojesus/vacancy_management/modules/company/repositories/JobRepository.java
@@ -0,0 +1,9 @@
+package br.com.pablojesus.vacancy_management.modules.company.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.pablojesus.vacancy_management.modules.job.entities.JobEntity;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID>{}
