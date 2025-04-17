7d3325839ca90cfe0f1bf236e63aa54da7a3af68
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2025-04-17 19:18:42 -0300

    :fix "fixing imports"

--- src/main/java/br/com/pablojesus/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/br/com/pablojesus/vacancy_management/VacancyManagementApplication.java
@@ -3,12 +3,6 @@ package br.com.pablojesus.vacancy_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-
 @SpringBootApplication
 public class VacancyManagementApplication {
 
